13ef14d3163211349db28a62f545104ecc9bb38d
Curtis Jensen <38483758+Curtis-Jensen@users.noreply.github.com>
2025-12-03 15:50:49 -0700

    Initial commit

---
+++ Assets/RoomManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class RoomManager : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log($"Connecting...");
+
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        base.OnConnectedToMaster();
+
+        Debug.Log($"Connected to Master");
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        base.OnJoinedLobby();
+
+        PhotonNetwork.JoinOrCreateRoom("main", null, null);
+
+        Debug.Log($"Joined Lobby");
+    }
+
+    public override void OnJoinedRoom()
+    {
+        base.OnJoinedRoom();
+
+        Debug.Log($"Joined Room: {PhotonNetwork.CurrentRoom.Name}");
+    }
+}
